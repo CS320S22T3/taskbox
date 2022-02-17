701e29113bd27ff8595be68e1cbede81201c7584
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-02-16 22:13:01 -0500

    feat: environment variables

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import { PORT } from './constants';
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(3000);
+app.listen(PORT);
