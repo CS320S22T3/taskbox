d8a1dadb539911a6b0216ae78c18c4da2f39bce1
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-02-22 01:25:15 -0500

    feat: sql connection

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -1,7 +1,11 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 
+import { assert } from "console";
 import { config } from "dotenv";
 
 config();
 
-export const PORT = process.env.PORT || 3000;
+export const EXPRESS_PORT = process.env.EXPRESS_PORT || 3000;
+
+export const SQL_CONNECTION_URI = process.env.SQL_CONNECTION_URI!;
+assert(SQL_CONNECTION_URI, "Missing `SQL_CONNECTION_URI` from env.");
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,8 @@
 import express from "express";
 import { Sequelize } from "sequelize";
-import { PORT } from "./constants";
+import { EXPRESS_PORT, SQL_CONNECTION_URI } from "./constants";
 
-const sequelize = new Sequelize({
-  dialect: "postgres",
-});
+const sequelize = new Sequelize(SQL_CONNECTION_URI);
 
 const app = express();
 app.use(express.json());
@@ -13,4 +11,7 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(PORT);
+sequelize
+  .authenticate()
+  .then(() => app.listen(EXPRESS_PORT))
+  .catch((rej) => console.error("Unable to connect to the database: ", rej));
