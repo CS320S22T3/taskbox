4b3a95b40ac1266c4164ea5997df2d81a6a47254
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-02-21 20:17:57 -0500

    feat: server formatting

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -1,11 +1,7 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 
-import assert from 'assert';
-import { config } from 'dotenv';
+import { config } from "dotenv";
 
 config();
 
 export const PORT = process.env.PORT || 3000;
-
-export const SQL_CONN_STR = process.env.SQL_CONN_STR!;
-assert(SQL_CONN_STR, 'No `SQL_CONN_STR` in `.env`.');
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,16 @@
-import express from 'express';
-import { PORT } from './constants';
+import express from "express";
+import { Sequelize } from "sequelize";
+import { PORT } from "./constants";
+
+const sequelize = new Sequelize({
+  dialect: "postgres",
+});
 
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
 app.listen(PORT);
