81672eba9a8f5d4e7d0b21c1e770dc35aaa30fa8
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-03-01 13:11:20 -0500

    fix: init_database script (#13)
    * fix: init_database script
    
    * fix: sudo as user
    
    * fix: taskbox instead of poopbox

--- init_database.sh
+++ init_database.sh
@@ -1,2 +1,2 @@
-sudo su -c "createuser -sdrPW $1" postgres
-sudo su -c "createdb -h localhost -p 5432 -U $1 taskbox" postgres
+sudo -u postgres createuser -sdrPW $1
+sudo -u postgres createdb -h localhost -p 5432 -U $1 taskbox
