52daeddf69cdbf4340da4f16b8d01261c87500d3
Ava S <absmith@umass.edu>
2022-03-07 16:26:43 -0500

    Added hooks for password encryption

--- server/db/models/users.js
+++ server/db/models/users.js
@@ -1,5 +1,7 @@
 'use strict';
 const { DataTypes } = require("sequelize"); // Import the built-in data types
+const bcrypt = require('bcrypt');
+
 const {
   Model
 } = require('sequelize');
@@ -16,23 +18,42 @@ module.exports = (sequelize, DataTypes) => {
   }
   users.init({
     id: {
-      type: DataTypes.INTEGER, 
-      primaryKey: true, 
+      type: DataTypes.INTEGER,
+      primaryKey: true,
       autoIncrement: true,
       unique: true,
-      validate:{isNull: false}},
+      validate: { isNull: false }
+    },
     email: {
-      type: DataTypes.CITEXT, 
+      type: DataTypes.CITEXT,
       unique: true,
-      validate:{isNull: false}},
+      validate: { isNull: false }
+    },
     password_digest: {
-      type: DataTypes.STRING, 
-      unique: true, 
-      validate:{isNull: false, len:[0,60]}}
-  }, 
-  {
-    sequelize,
-    modelName: 'users',
-  });
+      type: DataTypes.STRING,
+      unique: true,
+      validate: { isNull: false, len: [0, 60] }
+    }
+  },
+    {
+      hooks: {
+        beforeCreate: async (users) => {
+          if (users.password) {
+            const salt = await bcrypt.genSaltSync(10, 'a');
+            users.password = bcrypt.hashSync(users.password, salt);
+          }
+        },
+        beforeUpdate: async (users) => {
+          if (users.password) {
+            const salt = await bcrypt.genSaltSync(10, 'a');
+            users.password = bcrypt.hashSync(users.password, salt);
+          }
+        }
+      }
+    },
+    {
+      sequelize,
+      modelName: 'users',
+    });
   return users;
 };
