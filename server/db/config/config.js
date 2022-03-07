e713761eddcc9665e7dda1638c2860aa54174647
Ava S <absmith@umass.edu>
2022-03-07 15:18:53 -0500

    Adjusted config for dynamic input

--- server/db/config/config.js
+++ server/db/config/config.js
@@ -1,13 +1,13 @@
 /* Use username = < process.env.CI_DB_USERNAME > & password = < process.env.CI_DB_PASSWORD > */
 const fs = require('fs');
+const { env } = require('process');
 
 module.exports = {
   development: {
-    username: 'database_dev',
-    password: 'database_dev',
-    database: 'taskbox',
+    username: env.DB_USERNAME,
+    password: env.DB_PASSWORD,
+    database: env.DB_NAME,
     host: '127.0.0.1',
-    port: 5432,
     dialect: 'postgres',
     dialectOptions: {
       bigNumberStrings: true
--- server/db/seeders/demo-users.js
+++ server/db/seeders/demo-users.js
@@ -1,21 +1,21 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /*Populate table here*/
-     await queryInterface.bulkInsert('users', [{
-       id: 1,
-       email: "employee@development.com",
-       password_digest: "password", 
-       createdAt: new Date(),
-       updatedAt: new Date()
-     }], {});
+    await queryInterface.bulkInsert('users', [{
+      id: 1,
+      email: "employee@development.com",
+      password_digest: "$2y$10$aBVZQiZG/X3TbduZwdbJaOyBxLpVI/RYWjr5PxGnvinq/6xpXPowa",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /*Reverts seed*/
 
-     await queryInterface.bulkDelete('users', null, {});
-     
+    await queryInterface.bulkDelete('users', null, {});
+
   }
 };
