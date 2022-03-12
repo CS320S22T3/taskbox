781858f03878c148f4020abeb0c734457a0589fd
jeetmitra <jeetmitra01@gmail.com>
2022-03-11 19:39:54 -0500

    lint fix

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -2,16 +2,14 @@ import React from "react";
 import "./App.css";
 
 function Layout(props: { children: typeof React.Component }) {
-    return(
-        <div>
-            <header className="App-header">
-
-            </header>
-            <main>
-                <props.children />
-            </main>
-        </div>   
-    );
+  return (
+    <div>
+      <header className="App-header"></header>
+      <main>
+        <props.children />
+      </main>
+    </div>
+  );
 }
 
 export default Layout;
