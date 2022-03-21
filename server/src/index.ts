d4a83a16e4ec114ab9bcf5178314df47394e6a9f
Ava S <absmith@umass.edu>
2022-03-21 11:41:01 -0400

    Syntax correction for Lint server

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,9 +6,9 @@ import { EXPRESS_PORT, DB_NAME, DB_USERNAME, DB_PASSWORD } from "./constants";
   dialect: "postgres",
 });*/
 const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
-  host: "localhost"
-  , dialect: "postgres"
-});
+  host: "localhost",
+  dialect: "postgres"}
+  );
 
 const app = express();
 app.use(express.json());
