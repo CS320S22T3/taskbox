a7bb67f7e7a509b3082da85623f15d06797a81d2
Ava S <absmith@umass.edu>
2022-03-21 15:08:56 -0400

    Updated users model with sequelize.define(...)

--- server/db/models/users.js
+++ server/db/models/users.js
@@ -1,59 +1,38 @@
-'use strict';
-const { DataTypes } = require("sequelize"); // Import the built-in data types
+const { sequelize, DataTypes } = require("sequelize"); // Import the built-in data types
+const sequelize = new Sequelize('postgres::memory:');
 const bcrypt = require('bcrypt');
 
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class users extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  users.init({
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-      unique: true,
-      validate: { isNull: false }
-    },
-    email: {
-      type: DataTypes.CITEXT,
-      unique: true,
-      validate: { isNull: false }
-    },
-    password_digest: {
-      type: DataTypes.STRING,
-      unique: true,
-      validate: { isNull: false, len: [0, 60] }
-    }
+const users = sequelize.define('users', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    unique: true,
+    validate: { isNull: false }
   },
-    {
-      hooks: {
-        beforeCreate: async (users) => {
-          if (users.password) {
-            const salt = await bcrypt.genSaltSync(10, 'a');
-            users.password = bcrypt.hashSync(users.password, salt);
-          }
-        },
-        beforeUpdate: async (users) => {
-          if (users.password) {
-            const salt = await bcrypt.genSaltSync(10, 'a');
-            users.password = bcrypt.hashSync(users.password, salt);
-          }
-        }
+  email: {
+    type: DataTypes.CITEXT,
+    unique: true,
+    validate: { isNull: false }
+  },
+  password_digest: {
+    type: DataTypes.STRING,
+    unique: true,
+    validate: { isNull: false, len: [0, 60] }
+  }
+}, {
+  hooks: {
+    beforeCreate: async (users) => {
+      if (users.password) {
+        const salt = await bcrypt.genSaltSync(10, 'a');
+        users.password = bcrypt.hashSync(users.password, salt);
       }
     },
-    {
-      sequelize,
-      modelName: 'users',
-    });
-  return users;
-};
+    beforeUpdate: async (users) => {
+      if (users.password) {
+        const salt = await bcrypt.genSaltSync(10, 'a');
+        users.password = bcrypt.hashSync(users.password, salt);
+      }
+    }
+  }
+});
