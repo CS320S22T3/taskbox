2dc90681fd9f20342ab1f779793202d427b6a744
jeetmitra <jeetmitra01@gmail.com>
2022-03-23 17:38:20 -0400

    Added navbar component as a prop

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 
-function Layout(props: { children: typeof React.Component }) {
+function Layout(props: { children: typeof React.Component, navbar: typeof React.Component }) {
   return (
     <div>
-      <header className="App-header"></header>
+      <header className="App-header">
+        <props.navbar />
+      </header>
       <main>
         <props.children />
       </main>
