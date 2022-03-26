54c9b3e7495bbb5b70799ced58eeff05694fc48b
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-03-26 17:42:51 -0400

    chore: re-add configurable express port

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -5,15 +5,14 @@ import { config } from "dotenv";
 
 config();
 
-function getEnv(key: string): string {
-  assert(process.env[key], `.env file is missing the \`${key}\` field.`);
+function getEnv(key: string, doAssert = true): string {
+  if (doAssert)
+    assert(process.env[key], `.env file is missing the \`${key}\` field.`);
 
   return process.env[key]!;
 }
 
-export const EXPRESS_PORT = 3201;
-
-//export const SQL_CONNECTION_URI = getEnv("SQL_CONNECTION_URI");
+export const EXPRESS_PORT = getEnv("EXPRESS_PORT", false) || 3201;
 
 export const DB_NAME = getEnv("DB_NAME");
 export const DB_USERNAME = getEnv("DB_USERNAME");
