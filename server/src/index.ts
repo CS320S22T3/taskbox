59d504a9c709b12c7ee0421de342b56458190f31
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-03-27 18:07:42 -0400

    feat: server startup logging

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import session from "express-session";
 import { EXPRESS_PORT } from "./constants";
+import sequelize from "./db/sequelize";
 
 import { sessions } from "./routes/sessions.route";
 
@@ -9,8 +10,34 @@ app.use(express.json());
 app.use(session());
 
 const api = express.Router();
-app.use("/sessions", sessions);
+api.use("/sessions", sessions);
 
 app.use("/api", api);
 
-app.listen(EXPRESS_PORT);
+function start() {
+  return new Promise<void>((res, rej) => {
+    console.log("Authenticating with database...");
+    sequelize
+      .authenticate()
+      .then(() => {
+        console.log("Authenticated with database.");
+        console.log("Starting express server...");
+        app.listen(EXPRESS_PORT, () => {
+          console.log(`Express server listening on port ${EXPRESS_PORT}`);
+          res();
+        });
+      })
+      .catch(rej);
+  });
+}
+
+console.log("Starting server...");
+start()
+  .then(() => {
+    console.log("Server ready.");
+  })
+  .catch((e) => {
+    console.error("Unable to start server. Aborting.");
+    console.log(e);
+    process.exit(-1);
+  });
