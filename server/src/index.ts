22757f2a1233f80a3a3405fec28db60b4dea3913
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-03-27 23:19:57 -0400

    chore: remove herobrine

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,6 @@
 import express from "express";
 import session from "express-session";
-import { EXPRESS_PORT } from "./constants";
-import { SESSION_SECRET } from "./constants";
-import sequelize from "./db/sequelize";
+import { EXPRESS_PORT, SESSION_SECRET } from "./constants";
 
 import { sessions } from "./routes/sessions.route";
 
@@ -15,30 +13,4 @@ api.use("/sessions", sessions);
 
 app.use("/api", api);
 
-function start() {
-  return new Promise<void>((res, rej) => {
-    console.log("Authenticating with database...");
-    sequelize
-      .authenticate()
-      .then(() => {
-        console.log("Authenticated with database.");
-        console.log("Starting express server...");
-        app.listen(EXPRESS_PORT, () => {
-          console.log(`Express server listening on port ${EXPRESS_PORT}`);
-          res();
-        });
-      })
-      .catch(rej);
-  });
-}
-
-console.log("Starting server...");
-start()
-  .then(() => {
-    console.log("Server ready.");
-  })
-  .catch((e) => {
-    console.error("Unable to start server. Aborting.");
-    console.log(e);
-    process.exit(-1);
-  });
+app.listen(EXPRESS_PORT);
