9227fc8e37f11fa38f34962ca5fc23e58f915d61
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-03-28 16:43:10 -0400

    feat: default seed

--- server/db/seeds/default.js
+++ server/db/seeds/default.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcryptjs");
+
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
@@ -6,6 +8,15 @@ exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex("users").del();
   await knex("users").insert([
-    { email: "ben@email.com", }
+    { email: "ben@email.com", password_digest: bcrypt.hashSync("password") },
+    { email: "daniel@email.com", password_digest: bcrypt.hashSync("password") },
+    { email: "ava@email.com", password_digest: bcrypt.hashSync("password") },
+    { email: "matt@email.com", password_digest: bcrypt.hashSync("password") },
+    { email: "long@email.com", password_digest: bcrypt.hashSync("password") },
+    { email: "rohit@email.com", password_digest: bcrypt.hashSync("password") },
+    { email: "navid@email.com", password_digest: bcrypt.hashSync("password") },
+    { email: "aastha@email.com", password_digest: bcrypt.hashSync("password") },
+    { email: "jeet@email.com", password_digest: bcrypt.hashSync("password") },
+    { email: "hongxiang@email.com", password_digest: bcrypt.hashSync("password") },
   ]);
 };
