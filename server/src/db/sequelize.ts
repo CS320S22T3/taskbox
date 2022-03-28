cabd0fab9980ebe2c24d720f2da072fd6d516d25
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-03-27 22:51:28 -0400

    feat: create propper sequelize object

--- server/src/db/sequelize.ts
+++ server/src/db/sequelize.ts
@@ -1,3 +1,14 @@
 import { Sequelize } from "sequelize";
+import {
+  DB_HOST,
+  DB_NAME,
+  DB_PASSWORD,
+  DB_PORT,
+  DB_USERNAME,
+} from "../constants";
 
-export default new Sequelize();
+export default new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
+  host: DB_HOST,
+  dialect: "postgres",
+  port: isNaN(Number(DB_PORT)) ? Number(DB_PORT) : 5432,
+});
