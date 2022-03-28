c9e8821f54d7d4917ea876d922e21953fbc87f2e
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-03-27 22:27:17 -0400

    style

--- client/src/component/Auth.jsx
+++ client/src/component/Auth.jsx
@@ -4,69 +4,75 @@ import LoginForm from "../components/LoginForm";
 import Navbar from "../components/Navbar";
 
 class Auth extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { user: undefined };
-    }
+  constructor(props) {
+    super(props);
+    this.state = { user: undefined };
+  }
 
-    /**
-     * send a GET request to the server to set a default context when a user opens the
-     * web page, setting the context to a user if a session already exists
-     */
+  /**
+   * send a GET request to the server to set a default context when a user opens the
+   * web page, setting the context to a user if a session already exists
+   */
 
-    componentDidMount() {
-        fetch('/api/sessions', {
-            method: 'GET',
-            mode: 'cors',
-        }).then((res) => this.setState({ user: res.body }));
-    }
+  componentDidMount() {
+    fetch("/api/sessions", {
+      method: "GET",
+      mode: "cors",
+    }).then((res) => this.setState({ user: res.body }));
+  }
 
-    /**
-     * send a POST request to the server with the email and password submitted from
-     * the login form, set the context to the user if successful, otherwise log the
-     * error message to the console
-     */
+  /**
+   * send a POST request to the server with the email and password submitted from
+   * the login form, set the context to the user if successful, otherwise log the
+   * error message to the console
+   */
 
-    login(email, password) {
-        fetch('/api/sessions', {
-            method: 'POST',
-            mode: 'cors',
-            body: JSON.stringify({ email, password }),
-            headers: {'Content-Type': 'application/json'}
-        }).then((res) => {
-            switch (res.status) {
-                case 200:
-                    this.setState({ user: res.body })
-                    break;
-                case 400:
-                    break;
-                case 422:
-                    break;
-            }
-        }).catch(err => { 
-            console.log(err) 
-        })
-    };
+  login(email, password) {
+    fetch("/api/sessions", {
+      method: "POST",
+      mode: "cors",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => {
+        switch (res.status) {
+          case 200:
+            this.setState({ user: res.body });
+            break;
+          case 400:
+            break;
+          case 422:
+            break;
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-    /**
-     * send a DELETE request to the server to end the user's session and logout, remove that user
-     * from the context
-     */
+  /**
+   * send a DELETE request to the server to end the user's session and logout, remove that user
+   * from the context
+   */
 
-    logout() {
-        fetch('/api/sessions', {
-            method: 'DELETE',
-            mode: 'cors',
-        }).then((res) => this.setState({ user: undefined }))
-    };
+  logout() {
+    fetch("/api/sessions", {
+      method: "DELETE",
+      mode: "cors",
+    }).then((res) => this.setState({ user: undefined }));
+  }
 
-    render() {
-        return (
-            <userContext.Provider value={this.state.user}>
-                {!this.state.user ? (<Navbar onSubmit={this.logout}></Navbar>) : (<LoginForm onSubmit={this.login}/>)}
-            </userContext.Provider>
-        )
-    }
+  render() {
+    return (
+      <userContext.Provider value={this.state.user}>
+        {!this.state.user ? (
+          <Navbar onSubmit={this.logout}></Navbar>
+        ) : (
+          <LoginForm onSubmit={this.login} />
+        )}
+      </userContext.Provider>
+    );
+  }
 }
 
-export default Auth
+export default Auth;
