7789070d81657997cf7bffd39047383d9de580c1
Fish7793 <daveravab@gmail.com>
2022-03-27 21:53:09 -0400

    display auth element instead of react default

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import Auth from "./component/Auth";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth />
   </React.StrictMode>,
   document.getElementById("root")
 );
