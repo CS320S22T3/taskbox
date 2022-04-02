120a54a1ef87e1099a44ea674455d98f3873c662
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-04-02 17:51:08 -0400

    fix: re-add NavBar logic

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,13 +1,28 @@
 import React from "react";
 
-const navBar = () => {
-  return (
-    <>
-      <nav>
-        <button>Log Out</button>
-      </nav>
-    </>
-  );
-};
+interface NavbarParameters {
+  onSubmit: () => void;
+}
 
-export default navBar;
+class Navbar extends React.Component<NavbarParameters> {
+  constructor(props: NavbarParameters) {
+    super(props);
+
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  handleLogout(event: React.MouseEvent<HTMLButtonElement>) {
+    if (this.props.onSubmit != null) this.props.onSubmit();
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <nav>
+        <button onClick={this.handleLogout}>Log Out</button>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
