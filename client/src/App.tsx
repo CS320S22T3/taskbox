2fbd7a9e0c9f8150db3fd5daadacd54dde3c0b85
jeetmitra01 <70064142+jeetmitra01@users.noreply.github.com>
2022-04-02 17:13:48 -0400

    Main layout (#34)
    * Layout component with header and main
    
    * formatting changes
    
    * lint fix
    
    * Added navbar component as a prop
    
    * added navBar component to main-layout
    
    * Fixed issues with importing navbar in main-layout
    
    * updates
    
    * Fixed Layout's handling of props.children
    
    * Removed Test File
    
    * Fixed Test Client
    
    * Fixes Lint
    
    Co-authored-by: Ava S <absmith@umass.edu>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
-import "./App.css";
-import Navbar from "./components/Navbar.js";
+import Navbar from "./components/Navbar";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {<header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -19,7 +19,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+
+      </header>}
+
     </div>
   );
 }
