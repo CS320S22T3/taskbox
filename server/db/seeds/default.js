a50f2abe18d290dd2db3064305e9552672a3e1b9
Aastha Patil <aasthapatil@Aasthas-MacBook-Pro.local>
2022-04-04 00:32:46 -0400

    Rebased changes

--- server/db/seeds/default.js
+++ server/db/seeds/default.js
@@ -7,16 +7,21 @@ const bcrypt = require("bcryptjs");
 exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex("users").del();
+  await knex("companies").del();
+
+  await knex("companies").insert([
+    {name: "Development Company"}
+  ])
   await knex("users").insert([
-    { email: "ben@email.com", password_digest: bcrypt.hashSync("password") },
-    { email: "daniel@email.com", password_digest: bcrypt.hashSync("password") },
-    { email: "ava@email.com", password_digest: bcrypt.hashSync("password") },
-    { email: "matt@email.com", password_digest: bcrypt.hashSync("password") },
-    { email: "long@email.com", password_digest: bcrypt.hashSync("password") },
-    { email: "rohit@email.com", password_digest: bcrypt.hashSync("password") },
-    { email: "navid@email.com", password_digest: bcrypt.hashSync("password") },
-    { email: "aastha@email.com", password_digest: bcrypt.hashSync("password") },
-    { email: "jeet@email.com", password_digest: bcrypt.hashSync("password") },
-    { email: "hongxiang@email.com", password_digest: bcrypt.hashSync("password") },
+    { company_id: 1, email: "ben@email.com", password_digest: bcrypt.hashSync("password") },
+    { company_id: 1, email: "daniel@email.com", password_digest: bcrypt.hashSync("password") },
+    { company_id: 1, email: "ava@email.com", password_digest: bcrypt.hashSync("password") },
+    { company_id: 1, email: "matt@email.com", password_digest: bcrypt.hashSync("password") },
+    { company_id: 1, email: "long@email.com", password_digest: bcrypt.hashSync("password") },
+    { company_id: 1, email: "rohit@email.com", password_digest: bcrypt.hashSync("password") },
+    { company_id: 1, email: "navid@email.com", password_digest: bcrypt.hashSync("password") },
+    { company_id: 1, email: "aastha@email.com", password_digest: bcrypt.hashSync("password") },
+    { company_id: 1, email: "jeet@email.com", password_digest: bcrypt.hashSync("password") },
+    { company_id: 1, email: "hongxiang@email.com", password_digest: bcrypt.hashSync("password") },
   ]);
 };
