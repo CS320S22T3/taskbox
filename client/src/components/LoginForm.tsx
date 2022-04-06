7f4c775c92f2633811db86404d3db57d94571401
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-04-05 20:47:11 -0400

    fix: add change listeners

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -43,12 +43,20 @@ class LoginForm extends React.Component<LoginFormParameters, LoginFormState> {
       <Form>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            onChange={this.handleEmailChange}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onChange={this.handlePasswordChange}
+          />
         </Form.Group>
         <Button variant="primary" type="submit">
           Submit
