269f8d17f048d6ff4211dbba719ccf36b1d5461d
Aastha Patil <aasthapatil@1x-nat-vl930-172-30-109-123.wireless.umass.edu>
2022-04-11 15:17:16 -0400

    resolve merge conflicts

--- server/db/migrations/20220404035348_create_companies.js
+++ server/db/migrations/20220404035348_create_companies.js
@@ -5,17 +5,6 @@
 exports.up = function (knex) {
 
   return knex.schema
-<<<<<<< HEAD
-  .createTable("companies", (t) => {
-    t.increments("id").primary().unsigned();
-    t.string("name").notNullable()
-  })
-
-  .alterTable("users", (t) => {
-    t.integer("company_id").unsigned();
-    t.foreign("company_id").references("companies.id")
-  })
-=======
     .createTable("companies", (t) => {
       t.increments("id").primary().unsigned();
       t.string("name").notNullable()
@@ -25,9 +14,6 @@ exports.up = function (knex) {
       t.integer("company_id").unsigned().notNullable();
       t.foreign("company_id").references("companies.id")
     })
->>>>>>> 357b2e8ad807e6716ff4300222a416a6d7742b96
-
-
 };
 
 /**
