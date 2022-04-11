a62508291a1fcad9fd185db18250d113baf02455
jeetmitra <jeetmitra01@gmail.com>
2022-04-11 15:34:06 -0400

    added seeds

--- server/db/migrations/20220404193602_time_off_requests.js
+++ server/db/migrations/20220404193602_time_off_requests.js
@@ -8,6 +8,7 @@ exports.up = function (knex) {
     t.integer("type").notNullable();
     t.date("start_date").notNullable();
     t.date("end_date").notNullable();
+    t.text("notes").notNullable();
   });
 };
 
--- server/db/seeds/default.js
+++ server/db/seeds/default.js
@@ -8,6 +8,7 @@ exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex("users").del();
   await knex("tasks").del();
+  await knex("time_off_requests").del();
 
   const users = await knex("users").insert([
     { email: "ben@email.com", password_digest: bcrypt.hashSync("password") },
@@ -19,13 +20,39 @@ exports.seed = async function (knex) {
     { email: "navid@email.com", password_digest: bcrypt.hashSync("password") },
     { email: "aastha@email.com", password_digest: bcrypt.hashSync("password") },
     { email: "jeet@email.com", password_digest: bcrypt.hashSync("password") },
-    { email: "hongxiang@email.com", password_digest: bcrypt.hashSync("password") },
+    { email: "hongxiang@email.com", password_digest: bcrypt.hashSync("password"), },
   ]);
-
 
   //Date format as YYYY-MM-DD
   const tasks = await knex("tasks").insert([
-    { assigner_id: users[0].id, assignee_id: users[1].id, due_date: "2022-05-08", info_type: "time_off_requests", info_id: 1 }
+    { assigner_id: users[0].id, assignee_id: users[1].id, due_date: "2022-05-08", info_type: "time_off_requests", info_id: 1, },
   ]);
 
+  const time_off_requests = await knex("time_off_requests").insert([
+    {
+      type: 2,
+      start_date: "2022-06-08",
+      end_date: "2022-06-29",
+      notes: "spending summer break in europe",
+    },
+    {
+      type: 0,
+      start_date: "2022-05-12",
+      end_date: "2022-05-26",
+      notes:
+        "received a positive covid test have been asked to quarantine for 2 weeks",
+    },
+    {
+      type: 3,
+      start_date: "2022-09-01",
+      end_date: "2023-03-01",
+      notes: "parental leave",
+    },
+    {
+      type: 1,
+      start_date: "2022-09-01",
+      end_date: "2022-10-05",
+      notes: "jury duty",
+    },
+  ]);
 };
