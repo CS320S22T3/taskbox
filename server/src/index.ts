fe5c2afae17bb70a9ff53404d718601ca6e4adb1
rohitankit <rohit.michu@gmail.com>
2022-04-24 14:24:36 -0400

    testing routing

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,18 +5,25 @@ import { EXPRESS_PORT, SESSION_SECRET } from "./constants";
 import { sessions } from "./routes/sessions.route";
 import { tasks } from "./routes/tasks.route";
 
+
 const app = express();
 app.use(express.json());
+
 app.use(
   session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false })
 );
 
 const api = express.Router();
-api.use("/sessions", sessions);
 
-api.use("/tasks", sessions);
+app.use('/', (req, res)=> {
+  res.send('Hello World');
+})
 
-app.use("/api", api);
+// api.use("/sessions", sessions);
+
+app.use("/tasks", tasks);
+
+// app.use("/api", api);
 
 app.listen(EXPRESS_PORT, () => 
   console.log("Server listening on port " + EXPRESS_PORT)
--- server/src/routes/tasks.route.ts
+++ server/src/routes/tasks.route.ts
@@ -1,33 +1,40 @@
-import { body, CustomValidator } from 'express-validator';
+import { body } from 'express-validator';
 import { Request, Response, Router } from "express";
 import validate from "../middleware/validate";
 import { checkUserID } from "../models/users";
-import { Context } from "express-validator/src/context";
 
 export const tasks = Router();
 
-
-tasks.post(
-    "/:id",
-    validate([body("info_type").exists(), body("info_id").exists(), 
-    body("assigner_id").isNumeric(), body("assignee_id").isNumeric(), 
-    body("due_date").isDate(), body("created_date").isDate()]),
-    async (req: Request, res: Response) => {
-        return res.sendStatus(500);
-    // const { info_type, assigner_id, assignee_id } = req.body("assigner_id") as Record<Number, Number>;
-    // try {
-    //   const user = await checkUserID(assignee_id);
-    //   if (user && bcrypt.compareSync(password, user.password_digest)) {
-    //     req.session.user_id = String(user.id);
-    //     return res.status(200).json({ user_id: user.id });
-    //   } else {
-    //     return res.status(422).json({ error: "Incorrect email or password." });
-    //   }
-    // } catch (e) {
-    //   console.log("Error while trying to find user: " + e);
-
-    //   return res.sendStatus(500);
+tasks.get("/",
+    // async (req: Request, res: Response) => {
+    //     console.log("hi")
+    //     return res
+    //     .status(200);
     // }
-  }
-  
+    (req, res) => {
+        res.send('hello world1')
+    }
 )
+
+// tasks.post(
+//     "/",
+//     // validate([body("info_type").exists(), body("info_id").exists(), 
+//     // body("assigner_id").isNumeric(), body("assignee_id").isNumeric(), 
+//     // body("due_date").isDate(), body("created_date").isDate()]),
+//     async (req: Request, res: Response) => {
+//         console.log("hi")
+//     // const { info_type, assigner_id, assignee_id } = req.body("assigner_id") as Record<Number, Number>;
+//     // try {
+//     //   const user = await checkUserID(assignee_id);
+//     //   if (user && bcrypt.compareSync(password, user.password_digest)) {
+//     //     req.session.user_id = String(user.id);
+//     //     return res.status(200).json({ user_id: user.id });
+//     //   } else {
+//     //     return res.status(422).json({ error: "Incorrect email or password." });
+//     //   }
+//     // } catch (e) {
+//     //   console.log("Error while trying to find user: " + e);
+
+//       return res.sendStatus(200);
+//     }  
+// )
