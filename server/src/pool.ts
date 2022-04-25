052fda7e31d6ef8f64cd9330b934bcbc6011e827
Tengfei Louie <louie7679@gmail.com>
2022-04-25 15:16:40 -0400

    Create database table for user_informations. (#68)
    * Create database table for user_informations.
    
    * userinfo table
    
    * Update server/db/migrations/20220328193739_create_users.js
    
    * Delete package-lock.json
    
    * Delete package.json
    
    * fixed minor issues and typo
    
    * chained notnullable after each type
    
    * removed adding user_id to users
    
    * removed drop column
    
    * chore: foreign key
    
    * Update server/db/migrations/20220411191207_create_user_informations.js
    
    Co-authored-by: benmelz <ben.j.melz@gmail.com>
    
    * Update server/db/migrations/20220411191207_create_user_informations.js
    
    Co-authored-by: benmelz <ben.j.melz@gmail.com>
    
    * Update server/db/seeds/default.js
    
    Co-authored-by: benmelz <ben.j.melz@gmail.com>
    
    * Update server/db/seeds/default.js
    
    Co-authored-by: benmelz <ben.j.melz@gmail.com>
    
    Co-authored-by: Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
    Co-authored-by: benmelz <ben.j.melz@gmail.com>

--- server/src/pool.ts
+++ server/src/pool.ts
@@ -20,6 +20,16 @@ declare module "knex/types/tables" {
     users: User;
     companies: Company
   }
+
+  interface UserInformation {
+    id: number;
+    user_id: number;
+    first_name: string;
+    last_name: string;
+    position: string;
+    date_hired: Date;
+    is_manager: boolean;
+  }
 }
 
 export default knex(config);
