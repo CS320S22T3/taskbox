6c85b4bf01de0712e56c03b81f051635e9c58424
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-04-25 14:54:49 -0400

    style

--- server/src/models/tasks.ts
+++ server/src/models/tasks.ts
@@ -1,14 +1,14 @@
 import knex from "../pool";
 
 export async function createTask(newTask: any) {
-    return await knex('tasks')
-        .insert(newTask)
-        .returning('id')
-        .then(([{id}]) => {
-            newTask['id'] = id;
-            return newTask;
-        })
-        .catch((err) => {
-            throw new Error('Failed to create new Task ' + err);
-        })
+  return await knex("tasks")
+    .insert(newTask)
+    .returning("id")
+    .then(([{ id }]) => {
+      newTask["id"] = id;
+      return newTask;
+    })
+    .catch((err) => {
+      throw new Error("Failed to create new Task " + err);
+    });
 }
