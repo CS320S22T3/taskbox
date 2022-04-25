e439e2f70e5c2a0a74a08077210066589495f9f6
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-04-25 14:49:20 -0400

    style

--- server/src/routes/tasks.route.ts
+++ server/src/routes/tasks.route.ts
@@ -1,28 +1,48 @@
-import { body } from 'express-validator';
+import { body } from "express-validator";
 import { Request, Response, Router } from "express";
 import validate from "../middleware/validate";
 import { checkUserID } from "../models/users";
-import { createTask } from "../models/tasks"
+import { createTask } from "../models/tasks";
 import knex from "../pool";
 
 export const tasks = Router();
 
 tasks.post(
-    "/",
-    validate([body("info_type").exists(), body("info_id").exists(), 
-    body("assigner_id").isNumeric(), body("assignee_id").isNumeric(), 
-    body("due_date").isDate(), body("created_date").isDate()]),
-    async (req: Request, res: Response) => {
-        const { info_type, info_id, assigner_id, assignee_id, due_date, created_date } = req.body; 
-        try {
-            if (await checkUserID(assignee_id)) {
-                const newTask = {info_type, info_id, assigner_id, assignee_id, due_date, created_date};
-                const addedTask = await createTask(newTask);
-                return res.status(200).json(addedTask);
-            }
-        } catch(e) {
-            console.log(e)
-            return res.sendStatus(500);
-        }
+  "/",
+  validate([
+    body("info_type").exists(),
+    body("info_id").exists(),
+    body("assigner_id").isNumeric(),
+    body("assignee_id").isNumeric(),
+    body("due_date").isDate(),
+    body("created_date").isDate(),
+  ]),
+  async (req: Request, res: Response) => {
+    const {
+      info_type,
+      info_id,
+      assigner_id,
+      assignee_id,
+      due_date,
+      created_date,
+    } = req.body;
+    
+    try {
+      if (await checkUserID(assignee_id)) {
+        const newTask = {
+          info_type,
+          info_id,
+          assigner_id,
+          assignee_id,
+          due_date,
+          created_date,
+        };
+        const addedTask = await createTask(newTask);
+        return res.status(200).json(addedTask);
+      }
+    } catch (e) {
+      console.log(e);
+      return res.sendStatus(500);
     }
-)
+  }
+);
