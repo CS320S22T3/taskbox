6b60782db163b72ca2fccba836c19f3cdefbd426
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-04-25 04:46:06 -0400

    chore: foreign key

--- server/db/migrations/20220411191207_create_user_informations.js
+++ server/db/migrations/20220411191207_create_user_informations.js
@@ -2,22 +2,22 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-    return knex.schema.createTable("user_information", (t) => {
-        t.increments("id").primary().unsigned();
-        t.integer("user_id").unsigned().notNullable();
-        t.string("first_name").notNullable();
-        t.string("last_name").notNullable();
-        t.string("position").notNullable();
-        t.date("date_hired").notNullable();
-        t.boolean("is_manager").notNullable();
-    })
+exports.up = function (knex) {
+  return knex.schema.createTable("user_information", (t) => {
+    t.increments("id").primary().unsigned();
+    t.foreign("user_id").references("id").inTable("users").notNullable();
+    t.string("first_name").notNullable();
+    t.string("last_name").notNullable();
+    t.string("position").notNullable();
+    t.date("date_hired").notNullable();
+    t.boolean("is_manager").notNullable();
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-    knex.schema.dropTable("user_information");
+exports.down = function (knex) {
+  knex.schema.dropTable("user_information");
 };
