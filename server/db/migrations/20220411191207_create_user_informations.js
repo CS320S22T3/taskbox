6cb18a4cff2ab213a2c0f1fc530ab660cf54320b
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-04-25 14:58:28 -0400

    Update server/db/migrations/20220411191207_create_user_informations.js
    Co-authored-by: benmelz <ben.j.melz@gmail.com>

--- server/db/migrations/20220411191207_create_user_informations.js
+++ server/db/migrations/20220411191207_create_user_informations.js
@@ -19,5 +19,5 @@ exports.up = function (knex) {
  * @returns { Promise<void> }
  */
 exports.down = function (knex) {
-  knex.schema.dropTable("user_information");
+  knex.schema.dropTable("user_informations");
 };
