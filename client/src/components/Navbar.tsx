cdab05c8119c143b68bc37899f81497dba491ec9
Fish7793 <daveravab@gmail.com>
2022-04-27 00:57:50 -0400

    navbar styled

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,15 +1,33 @@
 import React from "react";
+import { Container, Navbar as Susbar, Button } from "react-bootstrap";
 import UserContext from "../context/UserContext";
 
 function Navbar() {
   return (
-    <UserContext.Consumer>
-      {(userContext) => (
-        <nav>
-          <button onClick={userContext?.logout}>Log Out</button>
-        </nav>
-      )}
-    </UserContext.Consumer>
+    <Susbar bg="dark" variant="dark">
+      <Container fluid={true}>
+        <Susbar.Brand>taskbox</Susbar.Brand>
+        <UserContext.Consumer>
+          {(userContext) => (
+            <Container
+              fluid={true}
+              style={{ display: "flex", justifyContent: "right" }}
+            >
+              <Button style={{ margin: "5px" }} variant="outline-success">
+                Assign Task
+              </Button>
+              <Button
+                onClick={userContext?.logout}
+                style={{ margin: "5px" }}
+                variant="outline-secondary"
+              >
+                Logout
+              </Button>
+            </Container>
+          )}
+        </UserContext.Consumer>
+      </Container>
+    </Susbar>
   );
 }
 
