ebaf43b94f1d45222b0fea6074737b8c97f72f38
rohitankit <rohit.michu@gmail.com>
2022-04-27 13:50:16 -0400

    .

--- server/src/models/tasks.ts
+++ server/src/models/tasks.ts
@@ -1,16 +1,25 @@
 import knex from "../pool";
 
-export async function createTask(newTask: any) {
+interface Task {
+  info_type: string,
+  info_id: number,
+  assigner_id: number,
+  assignee_id: number,
+  due_date: Date,
+  created_date: Date,
+  info: any
+}
 
-  const newInfo = await knex(newTask['info_type'])
+export async function createTask(newTask: Task) {
+  const newInfo = await knex(newTask["info_type"])
   .insert(newTask.info)
   .returning("id");
 
   const info_id = newInfo[0].id;
-
+  
   const {info, ...taskInfo} = newTask;
   
   return await knex("tasks")
-    .insert({...taskInfo, info_id})
-    .returning("id");
+  .insert({...taskInfo, info_id})
+  .returning("id");
 }
--- server/src/routes/tasks.route.ts
+++ server/src/routes/tasks.route.ts
@@ -11,20 +11,22 @@ tasks.post(
   validate([
     body("info_type").exists(),
     body("info_id").exists(),
-    body("assigner_id").isNumeric(),
-    body("assignee_id").isNumeric(),
+    body("assignee_id").isInt(),
     body("due_date").isDate(),
     body("created_date").isDate(),
     body("info").exists(),
   ]),
   async (req: Request, res: Response) => {
     try {
-      if (await checkUserID(req.body.assignee_id) && await checkUserID(req.body.assigner_id)) {
-        const addedTask = await createTask(req.body);
+
+      if (await checkUserID(req.body.assignee_id)) {
+          console.log(req.session);
+        req.body.assignee_id = req.session.user_id;
+        const addedTask = createTask(req.body);
         return res.status(200).json(addedTask);
       }
       else {
-        return res.status(422).json({ error: "Incorrect Assigner or Assignee" });
+        return res.status(422).json({ error: "Incorrect Assignee" });
       }
     } catch (e) {
       console.log(e);
