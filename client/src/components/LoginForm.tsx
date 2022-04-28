0c6e3df80f5973cb058145a49f73a55b7120b2cf
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-04-28 11:43:04 -0400

    feat: login form alert

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { Alert, Button, Form } from "react-bootstrap";
 
 interface LoginFormParameters {
   onSubmit: (username: string, password: string) => void;
+  error?: string;
 }
 
 interface LoginFormState {
@@ -33,12 +35,14 @@ class LoginForm extends React.Component<LoginFormParameters, LoginFormState> {
   handleSubmit(event: React.ChangeEvent<HTMLFormElement>) {
     if (this.props.onSubmit != null)
       this.props.onSubmit(this.state.email, this.state.password);
+
     event.preventDefault();
   }
 
   render() {
     return (
       <Form onSubmit={this.handleSubmit}>
+        {this.props.error && <Alert variant="danger">{this.props.error}</Alert>}
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control
