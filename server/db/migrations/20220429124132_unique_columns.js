23b137b9e128f645d62e9f90493ce60a679038dd
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-04-29 08:45:40 -0400

    feat: add unique column for tasks.info_id

---
+++ server/db/migrations/20220429124132_unique_columns.js
@@ -0,0 +1,15 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  knex.schema.alterTable("tasks", (table) => {
+    table.unique("info_id");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {};
