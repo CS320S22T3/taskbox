19bbf858946f90862dd16e7e923a413d308c8f2b
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-04-29 08:16:04 -0400

    style

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,12 +13,11 @@ app.use(
 );
 
 const api = express.Router();
-
 api.use("/sessions", sessions);
 api.use("/tasks", tasks);
 
 app.use("/api", api);
 
-app.listen(EXPRESS_PORT, () => 
+app.listen(EXPRESS_PORT, () =>
   console.log("Server listening on port " + EXPRESS_PORT)
 );
