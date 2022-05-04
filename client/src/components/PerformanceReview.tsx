6b7ff291bf8e07fd4995117a8a68e657f1037ed3
Matthew Crowe <52865353+matt532@users.noreply.github.com>
2022-05-04 15:49:36 -0400

    Unify task forms (#103)
    * first commit
    
    * implemented dynamic task form
    
    * moved assignee_id, assigner_id, and due_date to outer component
    
    * cleaned up code
    
    * fix lint warnings
    
    * fix lint warnings
    
    * removed form tag from TimeOffRequestForm component
    
    * fix: handleChange function in TaskForm
    
    * implemented single form component
    
    * edited TaskForm
    
    * fix: all fields get submitted
    
    * commented out TaskForm test
    
    * fix: package lock

--- client/src/components/PerformanceReview.tsx
+++ client/src/components/PerformanceReview.tsx
@@ -1,8 +1,10 @@
 import React from "react";
-import "../App.css";
 
 interface PerformanceReviewParameters {
   assigneeOptions: any;
+  assigner_id: number;
+  assignee_id: number;
+  due_date: string;
   onSubmit?: (
     assigner_id: number,
     assignee_id: number,
@@ -22,28 +24,12 @@ class PerformanceReview extends React.Component<
 > {
   constructor(props: any) {
     super(props);
-    this.handleDateChange = this.handleDateChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleAssigneeChange = this.handleAssigneeChange.bind(this);
     this.state = {
-      assigner_id: -1,
-      assignee_id: -1,
-      due_date: "",
+      assigner_id: props.assigner_id,
+      assignee_id: props.assignee_id,
+      due_date: props.due_date,
     };
-  }
-
-  handleAssigneeChange = (e: any) => {
-    console.log(this.state.assignee_id);
-    this.setState(() => ({
-      assignee_id: e.target.value,
-    }));
-  };
-
-  handleDateChange(e: any) {
-    console.log(this.state.due_date);
-    this.setState(() => ({
-      due_date: e.target.value,
-    }));
   }
 
   handleSubmit = (e: any) => {
@@ -61,28 +47,6 @@ class PerformanceReview extends React.Component<
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <h2>Performance Review Request</h2>
-        <br />
-        <label>
-          Asignee ID:
-          <select onChange={this.handleAssigneeChange}>
-            {this.props.assigneeOptions.map((assigneeOption: any) => (
-              <option key={assigneeOption.id} value={assigneeOption.id}>
-                {`${assigneeOption.first_name} ${assigneeOption.last_name}`}
-              </option>
-            ))}
-          </select>
-        </label>
-        <br />
-        <label>
-          Due Date:
-          <input
-            type="date"
-            value={this.state.due_date}
-            onChange={this.handleDateChange}
-          />
-        </label>
-        <br />
         <input type="submit" value="Submit" />
       </form>
     );
