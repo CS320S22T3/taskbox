d67c340bdf68a49b9b8ddf42ec38c273ce70b46f
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-05-04 15:16:29 -0400

    jank associated tasks function

--- server/src/models/tasks.ts
+++ server/src/models/tasks.ts
@@ -2,20 +2,24 @@ import knex from "../pool";
 import { TaskInfoInput, TaskInput } from "types";
 
 export async function getAssociatedTasksForUser(id: number) {
-  return await knex("tasks")
+  const associatedTasks = await knex("tasks")
     .where("assignee_id", "=", id)
-    .orWhere("assigner_id", "=", id)
-    .leftJoin(
-      "training_assignments",
-      "tasks.info_id",
-      "training_assignments.id"
-    )
-    .leftJoin("time_off_requests", "tasks.info_id", "time_off_request.id")
-    .leftJoin(
-      "performance_review_requests",
-      "tasks.info_id",
-      "performance_review_requests.id"
-    );
+    .orWhere("assigner_id", "=", id);
+
+  const associatedTaskIds = associatedTasks.map((row) => row.id);
+  const info = new Map<number, Record<string, unknown>>();
+  for (const table of [
+    "training_assignments",
+    "time_off_requests",
+    "performance_review_requests",
+  ])
+    for (const row of await knex(table).whereIn("id", associatedTaskIds))
+      info.set(row.id, row);
+
+  return associatedTasks.map((task) => ({
+    ...task,
+    info: info.get(task.info_id),
+  }));
 }
 
 export async function createTask(
