d13147d66afede3ce8c2d49d05f3101783bef0a2
Fish#7793 <55263331+Fish7793@users.noreply.github.com>
2022-05-04 14:39:01 -0400

    added users/get/ endpoint

--- server/src/models/users.ts
+++ server/src/models/users.ts
@@ -1,5 +1,11 @@
 import knex from "../pool";
 
+export async function getUsers() {
+  return knex("users")
+    .select("*")
+    .leftJoin("user_informations", "users.id", "user_informations.user_id");
+}
+
 export async function getUserFromEmail(email: string) {
   return knex("users").where({ email }).first();
 }
