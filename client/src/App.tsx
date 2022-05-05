53c3a5496610ee7005f6fb1e65e5c854c498cb8c
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2022-05-05 18:38:44 -0400

    fix task layer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,15 +2,28 @@ import React from "react";
 import Auth from "./components/SessionLayer";
 import Layout from "./components/Layout";
 import UserContext from "./context/UserContext";
+import TaskContext from "./context/TaskContext";
+import TaskLayer from "./components/TaskLayer";
 
 function App() {
   return (
     <Auth>
-      <Layout>
-        <UserContext.Consumer>
-          {(userContext) => `Logged in to user id ${userContext.user?.id}! 😎`}
-        </UserContext.Consumer>
-      </Layout>
+      <TaskLayer>
+        <Layout>
+          <UserContext.Consumer>
+            {(userContext) => `Logged in with user ${userContext.user?.id}.`}
+          </UserContext.Consumer>
+          <TaskContext.Consumer>
+            {(taskContext) =>
+              `They have the following tasks: ${Array.from(
+                taskContext.tasks.values()
+              )
+                .map((task) => JSON.stringify(task))
+                .join(", ")}.`
+            }
+          </TaskContext.Consumer>
+        </Layout>
+      </TaskLayer>
     </Auth>
   );
 }
